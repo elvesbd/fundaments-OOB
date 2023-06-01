4b60f3806bda575815fd24d679f13b32c96fb6f4
elvesbd <elvesbd@yahoo.com>
2023-06-01 20:24:29 -0300

    feat: modeling career

--- ContentContext/Career.cs
+++ ContentContext/Career.cs
@@ -2,6 +2,12 @@ namespace Balta.ContentContext
 {
     public class Career : Content
     {
-        public int Courses { get; set; }
+        public Career()
+        {
+            Items = new List<CareerItem>();
+        }
+
+        public IList<CareerItem> Items { get; set; }
+        public int TotalCourses => Items.Count;
     }
 }
